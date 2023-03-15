c13c7f0215fa003a8cbe701b947b8fdda1324a4a
XiaowanYi <48965338+XiaowanYi@users.noreply.github.com>
2023-03-15 22:36:48 +0000

    update with new out_dir and tqdm

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import torch
 from aegan import Generator as G
 import torchvision.utils as vutils
+import os
+from tqdm import tqdm
 
+num_of_pokemons = 20
+out_dir = 'NewPokemons'
 device = torch.device('cpu')
 netG = G()
 netG.load_state_dict(torch.load('trained_generator_weights.pt', map_location=device))
@@ -9,5 +13,8 @@ vec = torch.randn((32, 16))
 with torch.no_grad():
     fake = netG(vec)
 
-for i in range(32):
-    vutils.save_image(fake.data[i], f'testfake.{i:02d}.png', normalize=True)
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+    
+for i in tqdm(range(num_of_pokemons)):
+    vutils.save_image(fake.data[i], f'{out_dir}/creature.{i:02d}.png', normalize=True)
